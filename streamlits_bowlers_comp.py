d3737e55f31b2a677485cf0e027cecdb2daaa41e
adithya04dev <127680046+adithya04dev@users.noreply.github.com>
2023-08-17 17:06:12 +0530

    Update streamlits_bowlers_comp.py

--- streamlits_bowlers_comp.py
+++ streamlits_bowlers_comp.py
@@ -64,12 +64,12 @@ def main():
         sns.set(style="white")
         
         # Create the scatter plot
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(25, 15))
         scatter_plot = sns.scatterplot(y='average', x='runrate', data=result1)
         
         # Annotate the points with player names
         for i, row in result1.iterrows():
-            scatter_plot.text(row['runrate'],row['average'], row['player_name'], fontsize=8, alpha=0.7)
+            scatter_plot.text(row['runrate'],row['average'], row['player_name'], fontsize=18, alpha=0.7)
         
         plt.title('Scatter Plot of Economy vs Average')
         plt.xlabel('Economy')
